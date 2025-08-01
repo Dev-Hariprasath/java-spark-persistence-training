f31c13c4ea289929435f2efb26af8ad9ee70e469
Hariprasath <hariprasath0620@gmail.com>
2025-08-01 13:03:25 +0530

    updated

--- Day_08 (30-07-25)/Project/Java-8/src/main/java/org/revature/evenNumber/Main.java
+++ Day_08 (30-07-25)/Project/Java-8/src/main/java/org/revature/evenNumber/Main.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-        System.out.println("The Number List :");
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10, 12, 11);
+        System.out.println("The Number List : ");
         System.out.println(numbers);
 
         List<Integer> evenNumbers = numbers.stream().filter( n -> n % 2 == 0).toList();
